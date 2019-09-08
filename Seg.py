1eb1fd6f9742ae4f02245bf0b66f61fc052859a4
umutkalay <52309034+umutkalay@users.noreply.github.com>
2019-09-08 22:09:21 +0300

    Update Seg.py

--- Seg.py
+++ Seg.py
@@ -2,6 +2,7 @@
 # Convert a 7-segment number to an integer
 # seg fonksiyonuna sırası ile 1,2 ve 3. satırı giriyoruz.
 # We enter lines 1,2 and 3 respectively in seg function.
+# Örnek/example: seg("    _  _     _  _  _  _  _ ","  | _| _||_||_ |_   ||_||_|","  ||_  _|  | _||_|  ||_| _|") #123456789
 def seg(a,b,c):
     olası = []
     atlama = 0
